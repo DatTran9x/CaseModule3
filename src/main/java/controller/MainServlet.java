c4733e73b75e1acc08827d2ab713d9234f630c62
Admin <tran.x.dat1996@gmail.com>
2021-12-30 13:44:07 +0700

    Frist commit

---
+++ src/main/java/controller/MainServlet.java
@@ -0,0 +1,31 @@
+package controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "MainServlet", value = "/MainServlet")
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action){
+            default:
+                showList(request,response);
+        }
+    }
+
+    private void showList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("");
+        rd.forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
