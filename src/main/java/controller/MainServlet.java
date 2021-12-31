8f21acf95e824d4c55ef536712938a4e08ab3141
Admin <tran.x.dat1996@gmail.com>
2021-12-31 13:54:00 +0700

    CRUD

--- src/main/java/controller/MainServlet.java
+++ src/main/java/controller/MainServlet.java
@@ -5,8 +5,13 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "MainServlet", value = "/MainServlet")
+@WebServlet(name = "MainServlet", value = "/home")
 public class MainServlet extends HttpServlet {
+    private static final Show show = new Show();
+    private static final Add add = new Add();
+    private static final Edit edit = new Edit();
+    private static final Delete delete = new Delete();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
@@ -14,19 +19,70 @@ public class MainServlet extends HttpServlet {
             action = "";
         }
         switch (action){
+            case "showUserList":
+                show.showUserList(request,response);
+                break;
+            case "showUser":
+                show.showUser(request, response);
+                break;
+            case "showProduct":
+                show.showProduct(request, response);
+                break;
+            case "showAddUserForm":
+                show.showAddUserForm(request, response);
+                break;
+            case "showAddProductForm":
+                show.showAddProductForm(request, response);
+                break;
+            case "showEditUserForm" :
+                show.showEditUserForm(request, response);
+                break;
+            case "showEditAccountForm":
+                show.showEditAccountForm(request, response);
+                break;
+            case "showEditProductForm":
+                show.showEditProductForm(request, response);
+                break;
+            case "showDeleteUserForm":
+                show.showDeleteUserForm(request, response);
+                break;
+            case "showDeleteProductForm":
+                show.showDeleteProductForm(request, response);
+                break;
             default:
-                showList(request,response);
+            show.showProductList(request,response);
         }
     }
 
-    private void showList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        RequestDispatcher rd = request.getRequestDispatcher("");
-        rd.forward(request,response);
-    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action){
+            case "addUser":
+                add.addUser(request, response);
+                break;
+            case "addProduct":
+                add.addProduct(request,response);
+                break;
+            case "editUser":
+                edit.editUser(request, response);
+                break;
+            case "editAccount":
+                edit.editAccount(request, response);
+                break;
+            case "editProduct":
+                edit.editProduct(request, response);
+                break;
+            case "deleteUser":
+                delete.deleteUser(response, request);
+                break;
+            case "deleteProduct":
+                delete.deleteProduct(response, request);
+                break;
+        }
     }
 }
