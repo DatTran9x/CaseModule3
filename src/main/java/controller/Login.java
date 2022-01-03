dfda8f6d5e0a2cbbe4aa3c7fda7255a80f7c656b
Admin <tran.x.dat1996@gmail.com>
2022-01-03 21:27:42 +0700

    Main Case

---
+++ src/main/java/controller/Login.java
@@ -0,0 +1,40 @@
+package controller;
+
+import model.User;
+import service.UserService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class Login {
+    private static final UserService userService = new UserService();
+
+    void logIn(HttpServletResponse response, HttpServletRequest request) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password  = request.getParameter("password");
+        List<User> list =   userService.findAllUser();
+        RequestDispatcher requestDispatcher;
+        for (User user:list) {
+            if(user.getEmail().equals(username)&&user.getPassword().equals(password)){
+                requestDispatcher =  request.getRequestDispatcher("home");
+                requestDispatcher.forward(request,response);
+            }
+        }
+        requestDispatcher = request.getRequestDispatcher("/view/login.jsp");
+        requestDispatcher.forward(request,response);
+    }
+
+    void signIn(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password  = request.getParameter("password");
+        User user = new User(username,password);
+        userService.saveUser(user);
+        RequestDispatcher  requestDispatcher = request.getRequestDispatcher("/view/login.jsp");
+        requestDispatcher.forward(request,response);
+    }
+
+}
---
+++ src/main/java/controller/Payment.java
@@ -0,0 +1,22 @@
+package controller;
+
+import service.CartService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Payment {
+    private static final CartService cartService = new CartService();
+
+    void payCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id_user = Integer.parseInt(request.getParameter("id_user"));
+        cartService.deleteCart(id_user);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/view/cart.jsp");
+        requestDispatcher.forward(request,response);
+    }
+
+
+}
